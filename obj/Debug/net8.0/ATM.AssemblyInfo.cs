197c8c4f3fabebb96a54406450f67653aeb6f56a
alosuri <alosuri@fedora.home>
2024-05-22 19:01:23 +0200

    Update pliku .json

--- obj/Debug/net8.0/ATM.AssemblyInfo.cs
+++ obj/Debug/net8.0/ATM.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ATM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1b18a981c860992bec7a29de81ac34c89824c14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54122716943a389613fb04b80a9ea8e70e7044c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ATM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ATM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
